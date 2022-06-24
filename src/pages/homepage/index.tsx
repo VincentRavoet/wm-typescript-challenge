06db71b65eca8f0c5624346603d0891c4fbabc39
Vincent Ravoet <vincentravoet@hotmail.com>
2022-06-24 18:01:03 +0200

    wieni-1: lelijke versie van de all recipes pagina

--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -15,7 +15,7 @@ const Homepage = () => {
     <main>
       <div className="mb-4 grid grid space-y-3 rounded border border-gray-200 bg-white/25 p-5 text-sm dark:bg-gray-100 sm:grid-cols-2 lg:grid-cols-4">
         {items.slice(0, 5).map((recipe: Recipe) => (
-          <Card recipe={recipe} />
+          <Card key={recipe.name} recipe={recipe} />
         ))}
       </div>
     </main>
--- src/pages/recipes/index.tsx
+++ src/pages/recipes/index.tsx
@@ -3,30 +3,43 @@ import { useState } from "react";
 import { Recipe } from "types/Recipe";
 
 const Recipes = () => {
-  let offset: number = 0;
-
+  const [offset, setOffset] = useState<number>(0);
+  const URL = `http://localhost:3000/api/recipes/all?limit=8&offset=${
+    offset * 8
+  }`;
   const [items, setItems] = useState<Recipe[]>([]);
-  const getPage = () => {
-    fetch(`http://localhost:3000/api/recipes/all?limit=8&offset=${offset}`)
-      .then((response) => response.json())
-      .then(({ cocktails }) => setItems(cocktails));
-  };
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+  const [totalItems, setTotalItems] = useState<number>(0);
 
-  getPage();
+  const handleFetch = () => {
+    fetch(URL)
+      .then((response) => response.json())
+      .then(({ data, total }) => {
+        setItems(data);
+        setTotalItems(total);
+        setIsLoaded(true);
+      });
+  };
 
   return (
     <div className="space-y-3 rounded border border-gray-200 bg-white/25 p-5 text-sm">
       <div className="mb-4 grid space-y-3 rounded border border-gray-200 bg-white/25 p-5 text-sm dark:bg-gray-100 sm:grid-cols-2 lg:grid-cols-4">
-        {items?.map((recipe: Recipe) => (
-          <Card recipe={recipe} />
-        ))}
+        {isLoaded ? (
+          items.map((recipe: Recipe) => (
+            <Card key={recipe.name} recipe={recipe} />
+          ))
+        ) : (
+          <div>Press the button to load the recipes!</div>
+        )}
       </div>
+
+      <p>Page {offset}</p>
 
       <button
         type="button"
         onClick={() => {
-          offset = offset > 0 ? (offset -= 1) : offset;
-          getPage();
+          setOffset(offset > 0 ? offset - 1 : 0);
+          handleFetch();
         }}
       >
         Previous page
@@ -35,8 +48,8 @@ const Recipes = () => {
       <button
         type="button"
         onClick={() => {
-          offset += 1;
-          getPage();
+          setOffset(offset * 8 > totalItems ? offset : offset + 1);
+          handleFetch();
         }}
       >
         Next page
